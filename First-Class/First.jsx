69d47089177d952650c90a464edf6fc4ca1a6b00
Shambhavisharma13 <shambhavisharma8235@gmail.com>
2026-02-22 16:43:13 +0530

    Add files via upload

---
+++ First-Class/First.jsx
@@ -0,0 +1,17 @@
+function First(){
+    return(
+        <h1>heyyy i am Shambhavi!! </h1>
+    )
+}
+export default First;
+
+ export function First1(){
+    return(
+        <h1>I am learning React</h1>
+    )
+}
+ export function First2(){
+    return(
+        <h1>React is tough library</h1>
+    )
+}
